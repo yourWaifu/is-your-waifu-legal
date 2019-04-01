720008c1c108a3aa81e0c06781ff22f9ed851d77
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 19:10:44 +0000

    Deploy yourWaifu/is-your-waifu-legal to github.com/yourWaifu/is-your-waifu-legal.git:gh-pages

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ function displayWaifuStats(name) {
         history.replaceState(historyState, "", query);
     }
     let request = new XMLHttpRequest();
-    request.open("GET", getCurrentURL() + "/waifus/" + input.toLowerCase() + ".json");
+    request.open("GET", getCurrentURL() + "waifus/" + input.toLowerCase() + ".json");
     request.responseType = "json";
     request.onerror = function (event) {
         console.log(event);
